a0c3ccfc56e21f68f532c94ed80be93e2a904b4a
Marcos Vinicius <marcosfp2021@hotmail.com>
2024-10-21 17:48:13 -0300

    atualizando

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'background': "url('/img/complementos/bg.jpg')",
+        'background': "url('./img/complementos/bg.jpg')",
       
       },
     },
