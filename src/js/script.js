fa6e398130546c2a249fd9aaa6e2928d29a594d6
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2024-10-31 19:31:51 -0300

    slider principal com js

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+function sliderPrincipal(){
+    let botoes = document.querySelectorAll(".pointers");
+    let slider = document.querySelector(".sliders");
+    botoes.forEach((botao, index) => {
+        botao.setAttribute('data-index', index);
+        botao.addEventListener('click', trocar);
+});
+
+function trocar(e) {
+    botoes.forEach((botao) => botao.classList.remove('ativo'));
+    e.currentTarget.classList.add('ativo');
+    let index = e.currentTarget.getAttribute('data-index');
+    slider.style.marginLeft = `${index * -100}vw`;
+    console.log(e)
+}
+
+
+}
+
+sliderPrincipal()
