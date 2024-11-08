388c35e0e334cb0f931a6498dc8d940c5f5a5c64
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2024-11-08 19:19:26 -0300

    implementação do ultimo slider

--- src/js/script.js
+++ src/js/script.js
@@ -19,3 +19,27 @@ function trocar(e) {
 }
 
 sliderPrincipal()
+
+
+function sliderHappy(){
+    let botoes = document.querySelectorAll(".pointersHappy");
+    let slider = document.querySelector(".slidersHappyClients");
+    slider.style.width=`${100*botoes.length}vw`
+    botoes.forEach((botao, index) => {
+        botao.setAttribute('data-index', index);
+        botao.addEventListener('click', trocar);
+});
+
+function trocar(e) {
+    botoes.forEach((botao) => botao.classList.remove('ativo'));
+    e.currentTarget.classList.add('ativo');
+    let index = e.currentTarget.getAttribute('data-index');
+    slider.style.marginLeft = `${index * -100}vw`;
+    console.log(e)
+}
+
+
+}
+
+sliderHappy()
+
